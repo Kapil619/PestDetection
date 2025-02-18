ac2e826f7d1af25ccdcbffc0d7ae35474764e112
Kapil619 <kapilbadokar321@gmail.com>
2025-02-18 10:02:06 +0530

    Created Tab navigator and create screens.

--- src/navigation/Main.tsx
+++ src/navigation/Main.tsx
@@ -1,25 +1,83 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Search from "../screens/Search";
+import Login from "../screens/Login";
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({
+          focused,
+          color,
+          size,
+        }: {
+          focused: boolean;
+          color: string;
+          size: number;
+        }) => {
+          let iconName: keyof typeof Ionicons.glyphMap = "home";
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Search") {
+            iconName = focused ? "search" : "search-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person" : "person-outline";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              size={size}
+              color={focused ? "green" : color}
+            />
+          );
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{ headerShown: false }}
+      />
+
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+};
+
 const Main = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Main">
         <Stack.Screen
-          name="Home"
-          component={Home}
+          name="Main"
+          component={TabNavigator}
           options={{
-            title: "Home",
+            headerShown: false,
           }}
         />
         <Stack.Screen
-          name="About"
-          component={Profile}
+          name="Login"
+          component={Login}
           options={{
-            title: "Profile",
+            headerShown: false,
           }}
         />
       </Stack.Navigator>
